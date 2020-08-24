d7d803923dff0556b473f5e7c1d5c9017cdda897
Bhaskar <67137854+hkbhaskar@users.noreply.github.com>
2020-08-24 11:59:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 score = 0;
 cross = true;
 
-audio = new Audio('music.mp3');
-audiogo = new Audio('gameover.mp3');
+audio = new Audio('iDragon-Adventures/music.mp3');
+audiogo = new Audio('iDragon-Adventures/gameover.mp3');
 setTimeout(() => {
     audio.play()
 }, 1000);
